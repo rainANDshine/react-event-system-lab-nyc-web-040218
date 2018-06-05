bcde02199c3836b2b5404ac0d52eb9d8d2822c7a
Shun Yao <ysmiracle@hotmail.com>
2018-06-05 03:02:19 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React, {Component} from 'react';
+
+export default class Keypad extends React {
+  tickle = () => {
+    console.log('Entering password...');
+  }
+  
+  render() {
+    return (
+      <input type="password" onClick={this.tickle}/>
+    )
+  }
+};
+
